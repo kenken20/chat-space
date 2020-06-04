30e441e2c3f5b3d7a415b9307402061ed515549a
kenken20 <techshi3@gmail.com>
2020-06-04 09:49:45 +0900

    automatic_updata

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function() {
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message-contents">
+       `<div class="message-contents" data-message-id=${message.id}>
           <div class="message-contents__info">
             <div class="message-name">
               ${message.user_name}
@@ -19,7 +19,7 @@ $(function() {
       return html;
     } else {
       var html =
-       `<div class="message-contents">
+       `<div class="message-contents" data-message-id=${message.id}>
           <div class="message-contents__info">
             <div class="message-name">
               ${message.user_name}
@@ -61,4 +61,30 @@ $(function() {
       $('.input-box__send-btn').prop('disabled', false);
     })
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message-contents:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
